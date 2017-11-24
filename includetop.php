2a585f3bc358970c548ca9da64d54afcf33d6576
Juthawong Naisanguansee <juthawong@gmail.com>
2017-11-24 20:43:52 +0700

    Update includetop.php

--- includetop.php
+++ includetop.php
@@ -1,5 +1,18 @@
 <?php
-ob_start();
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
+ob_start(function($output){
+    if( (strpos($output,"<video") > -1 || strpos($output,"<audio") > -1  || strpos($output,"<source") > -1 )  && (strpos($output,"<safe") == FALSE) ){
+    //Check If There is Video On The Page Then Load Defa Protector
+    // Source Tag Validation isn't need but for safety 
+    //If HTML Contains Safe Tag, Then Not Load Defa Protector
+        
+        $output = preg_replace_callback("/(^(<video|<audio|<source)[^>]*src *= *[\"']?)([^\"']*)/i", function ($matches) {
+            $crc = substr(sha1($matches['3']), -8, -1);
+            $_SESSION['defaprotect'.$crc] = $matches['3'];
+            return $matches[1] . "/defavid.php?crc=".$crc;
+      } , $output);
+    }
+    return $output;
+});
