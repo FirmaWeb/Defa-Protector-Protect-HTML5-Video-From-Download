e0ad015748288082e47bdc9341cc9fbe25ca8222
Juthawong Naisanguansee <juthawong@gmail.com>
2017-11-24 20:09:03 +0700

    Update includebottom.php

--- includebottom.php
+++ includebottom.php
@@ -1,14 +1,14 @@
 <?php
 $output = ob_get_clean();
-if( (strpos($output,"<video") || strpos($output,"<audio")  || strpos($output,"<source") )  && !(strpos($output,"<safe")) ){
+if( (strpos($output,"<video") > -1 || strpos($output,"<audio") > -1  || strpos($output,"<source") > -1 )  && (strpos($output,"<safe") == FALSE) ){
 //Check If There is Video On The Page Then Load Defa Protector
 // Source Tag Validation isn't need but for safety 
 //If HTML Contains Safe Tag, Then Not Load Defa Protector
-    function getURL($matches) {
-          $crc = substr(sha1($matches['2']), -8, -1);
-          $_SESSION['defaprotect'.$crc] = sha1($crc).base64_encode($matches['2']).sha1($crc);
-          return $matches[1] . $rootURL . "/defavid.php?crc=".$crc;
-    }
-    $output = preg_replace_callback("/(^(<video|<audio|<source)[^>]*src *= *[\"']?)([^\"']*)/i", getURL, $output);
+    
+    $output = preg_replace_callback("/(^(<video|<audio|<source)[^>]*src *= *[\"']?)([^\"']*)/i", function ($matches) {
+        $crc = substr(sha1($matches['3']), -8, -1);
+        $_SESSION['defaprotect'.$crc] = $matches['3'];
+        return $matches[1] . "/defavid.php?crc=".$crc;
+  } , $output);
 }
 echo $output;
