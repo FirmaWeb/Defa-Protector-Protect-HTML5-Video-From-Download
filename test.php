7e81b8bbfa15437c3cc1be546ce720f7f4056d64
Juthawong Naisanguansee <juthawong@gmail.com>
2017-11-24 21:05:23 +0700

    Update test.php

--- test.php
+++ test.php
@@ -3,11 +3,10 @@ include('includetop.php');
 ?>
 <video src='small.mp4'></video>
 
-P.s. defavid.php should be on the root of your subdomain.
-
-For Example :
-
-http://somewebdomainorwww.ampareengine.com/somedirectorythatbefore/yourhtml/test.php
-
-YOu should put defavid on :
+<div>
+P.s. defavid.php should be on the root of your subdomain.<br>
+For Example :<br>
+http://somewebdomainorwww.ampareengine.com/somedirectorythatbefore/yourhtml/test.php<br>
+YOu should put defavid on :<br>
 http://somewebdomainorwww.ampareengine.com/defavid.php
+</div>
