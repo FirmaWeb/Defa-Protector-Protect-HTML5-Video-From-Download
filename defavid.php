16c7f3b53f474006e80d6f5f01148461ff3bf3f0
Juthawong Naisanguansee <juthawong@gmail.com>
2018-03-07 23:13:24 +0700

    Update defavid.php

--- defavid.php
+++ defavid.php
@@ -4,16 +4,18 @@ if (session_status() == PHP_SESSION_NONE) {
 }
 ob_start();
 $crc = filter_var($_GET['crc']);
-$file = $_SESSION['defaprotect'.$crc];
-if( $headerurl = @get_headers($file,1)['Location'] ){
- $file = $headerurl;
-}
-if(isset($_SERVER['HTTP_RANGE'])){
-        $opts['http']['header']="Range: ".$_SERVER['HTTP_RANGE'];
-        $opts['http']['method']= "GET";
-        $cong= stream_context_create($opts);
-        ob_end_clean();
-        readfile($file,false,$cong);
-        exit();
+$file = $_SESSION['defaprotect' . $crc];
+if ($headerurl = @get_headers($file, 1)['Location']) {
+    if (!empty($headerurl)) {
+        $file = $headerurl;
     }
 
+}
+if (isset($_SERVER['HTTP_RANGE'])) {
+    $opts['http']['header'] = "Range: " . $_SERVER['HTTP_RANGE'];
+    $opts['http']['method'] = "GET";
+    $cong = stream_context_create($opts);
+    ob_end_clean();
+    readfile($file, false, $cong);
+    exit();
+}
